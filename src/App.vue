e6943d9706fd750e8125f2d1087bc4aa9630b384
lirui <liruiruilitwo@163.com>
2017-12-05 21:15:06 +0800

    app.vue及main.js

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <transition name="router-fade" mode="out-in">
+      <keep-alive>
+        <router-view v-if="$route.meta.keepAlive"></router-view>
+      </keep-alive>
+    </transition>
+    <transition name="router-fade" mode="out-in">
+      <router-view v-if="!$route.meta.keepAlive"></router-view>
+    </transition>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'app'
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  .router-fade-enter-active, .router-fade-leave-active {
+    transition: opacity .2s;
+  }
+  .router-fade-enter, .router-fade-leave-active {
+    opacity: 0;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import routes from './router/router'
+import store from './vuex/'
+import './style/public.scss'
+import './config/rem'
+import FastClick from 'fastclick'
 
-Vue.config.productionTip = false
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body);
+  }, false);
+}
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+Vue.use(VueRouter);
+
+const  router=VueRouter({
+  routes,
+  mode:'history',
+  strict:process.env.NODE_ENV!=='production',
+  scrollBehavior(to, from, savedPosition) {
+    if(savedPosition){
+      return savedPosition
+    }else {
+      return {
+        x:0,
+        y:to.meta.savedPosition||0
+      }
+    }
+  }
+});
